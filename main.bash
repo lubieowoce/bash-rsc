b423da164fa6adbbae1205554a42986a7da433d6
lubieowoce <lolzatu2@gmail.com>
2024-02-18 02:13:44 +0100

    recursive rendering

--- main.bash
+++ main.bash
@@ -5,6 +5,15 @@ function intoJq {
   echo null | jq '$value' --arg value "$1"
 }
 
+function get {
+  local args=( "$@" )
+  echo "$1" | jq "${args[@]:2}" ".$2"
+}
+
+function text {
+  intoJq "$1"
+}
+
 function jsx {
   local component;
   component="$1";
@@ -15,21 +24,25 @@ function jsx {
 
   props="$({
     echo '{'
-    local propName
-    local propVal
+    local propName; local propVal
     local length=${#args_sliced[@]}
     local last_ix=$((length-1))
-    local kv
+    local kv;
+    local kvFirst; local nameLength; local sliceLength
     local line
     for ((i=0;i<length;i++)); do
       kv="${args_sliced[$i]}"
-      kv=$(echo "$kv" | tr '\n' ' ')
-      propName=$(echo "$kv" | sed -nE 's/^([^=]+)=(.*)/\1/p')
-      propVal=$(echo "$kv" | sed -nE 's/^([^=]+)=(.*)/\2/p')
+      kvFirst=$(echo "$kv" | head)
+      propName=$(echo "$kvFirst" | sed -nE 's/^([^=]+)=(.*)/\1/p')
+      
+      nameLength=${#propName}
+      sliceLength=$((nameLength+1))
+      propVal="${kv:$sliceLength}"
+
       if [ "$propName" = "children" ]; then
-        propVal=$(echo "$propVal" | jq --slurp || intoJq "$propVal")
+        propVal=$(echo "$propVal" | jq --slurp 2>/dev/null || intoJq "$propVal")
       else
-        propVal=$(echo "$propVal" | jq || intoJq "$propVal")
+        propVal=$(echo "$propVal" | jq 2>/dev/null || intoJq "$propVal")
       fi
       line="$(intoJq "$propName"): $propVal"
       if [ $i -ne $last_ix ]; then
@@ -43,7 +56,88 @@ function jsx {
   echo "{ \"type\": \"$component\", \"props\": $props }" | jq
 }
 
-jsx MyComponent x=1 y=2 children="$({
-  jsx Child i=1
-  jsx Child i=2
-})"
+# =============================================================
+
+
+function isHostElement {
+  [[ "$1" =~ ^[a-z]+$ ]]
+}
+
+function renderNode {
+  local element="$1"
+  
+  local kind;
+  kind="$(echo "$element" | jq -r '. | type')" # string, number, boolean, null, object, array
+  # log "renderNode[$kind]: $element" >&2
+  case "$kind" in
+    string|number|boolean|null)
+      echo "$element"
+      return 0
+      ;;
+    array)
+      # log "renderNode[array]: $element" >&2
+      local length
+      length=$(echo "$element" | jq '. | length')
+      {
+        for ((i=0;i<length;i++)); do
+          renderNode "$(get "$element" "[$i]")"
+        done
+      } | jq -s
+      return 0
+      ;;
+    object)
+      renderElement "$element"
+      return 0
+  esac
+}
+
+function log {
+  echo "$@" >&2
+}
+
+function renderElement {
+  local element="$1"
+  local componentType; local componentProps
+  componentType="$(get "$element" 'type' -r)"
+  componentProps="$(get "$element" 'props')"
+  
+  if isHostElement "$componentType"; then
+    local componentChildren
+    componentChildren=$(get "$componentProps" 'children' || echo '[]')
+    local childrenRendered
+    childrenRendered="$(renderNode "$componentChildren")"
+    # log "renderElement[$componentType] children: $componentChildren of '$componentType' rendered: $childrenRendered" >&2
+    echo "$element" | jq '.props.children=$newChildren' --argjson newChildren "$childrenRendered"
+  else
+    local rendered
+    rendered="$($componentType "$componentProps")"
+    renderNode "$rendered"
+  fi
+}
+
+# =============================================================
+
+function MyComponent {
+  local props="$1"
+  jsx div children="$(
+    text "Hello from MyComponent, x is $(get "$props" 'x')"
+    jsx Child i=0
+    get "$props" 'children'
+  )"
+}
+
+function Child {
+  local props="$1"
+  jsx span children="$(
+    text "Hello from Child number $(get "$props" 'i')"
+  )"
+}
+
+tree="$(
+  jsx MyComponent x=1 y=2 children="$({
+    jsx Child i=1
+    jsx Child i=2
+  })"
+)"
+
+renderNode "$tree"
